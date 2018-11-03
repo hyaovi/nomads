de6d1fc380ff6d5b8b9dc6f67c24819a56cb0d43
hyaovi <hfyakre@gmail.com>
2018-11-04 02:02:36 +0300

    add token generation on login

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+const PASSWORD = "jplaLOL.1";
+module.exports = {
+  mongodbURI: `mongodb+srv://yaovi:${PASSWORD}@le-cluster-gb95k.mongodb.net/cities?retryWrites=true`,
+  secret: "joker"
+};
--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const User = require("../../models/Users");
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const keys = require("../../config/keys");
+
 //@route  GET api/users/tests
 // @description tests users routes
 // @acesss Public
@@ -16,10 +19,11 @@ router.get("/test", (req, res) => {
 router.post("/register", (req, res) => {
   const email = req.body.email;
   const name = req.body.name;
+  const password = req.body.password;
   const avatar = gravatar.url(req.body.avatar, {
     size: "200",
     rating: "pg",
-    default: "dd"
+    default: "mm"
   });
   User.findOne({ email }).then(user => {
     if (user) {
@@ -29,7 +33,7 @@ router.post("/register", (req, res) => {
         name,
         email,
         avatar,
-        password: req.body.password
+        password
       });
       bcrypt.genSalt(10, (err, salt) => {
         bcrypt.hash(newUser.password, salt, (err, hash) => {
@@ -40,6 +44,49 @@ router.post("/register", (req, res) => {
           });
         });
       });
+    }
+  });
+});
+
+//@route  GET api/users/login
+// @description checks the user credentials and return a token
+// @acesss Public
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({ email }, (err, user) => {
+    if (err) {
+      res.json({ message: err });
+    }
+    if (!user) {
+      res.status(404).json({ email: "user not found" });
+    } else {
+      bcrypt
+        .compare(password, user.password)
+        .then(sucess => {
+          if (sucess) {
+            //password matched!
+            const payload = {
+              id: user.id,
+              name: user.name,
+              avatar: user.avatar
+            };
+            jwt.sign(
+              payload,
+              keys.secret,
+              { expiresIn: 3600 },
+              (err, token) => {
+                if (err) {
+                  console.log(err);
+                }
+                res.json({ message: "success", bearer: `Bearer  ${token}` });
+              }
+            );
+          } else {
+            res.json({ message: "password incorrect" });
+          }
+        })
+        .catch(err => res.json({ message: err }));
     }
   });
 });
