0186fa26238c02af1b50ad02a688c862ef637e98
hyaovi <hfyakre@gmail.com>
2018-11-27 00:34:48 +0300

    Users.js in model got renamed into User.js, First route for profile rounting created

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+//Load profile model
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
 
 //@route  GET api/profile/tests
 // @description tests profile routes
@@ -7,4 +13,25 @@ const router = express.Router();
 router.get("/test", (req, res) => {
   res.json({ message: "profile works!" });
 });
+
+//@route  GET api/profile/
+// @description get the current user profile
+// @acesss Private
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const error = {};
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        if (!profile) {
+          error.noprofile = "this user has no profile yet";
+          res.status(404).json(error);
+        }
+        res.json(profile);
+      })
+      .catch(err => res.json(err));
+  }
+);
+
 module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../../models/Users");
+const User = require("../../models/User");
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
