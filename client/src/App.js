d4a48a4eedf1986fa06be737fa5d11eeaf7996c3
hyaovi <hfyakre@gmail.com>
2018-12-04 00:35:31 +0300

    implementing front-end client using react and bootstrap, just added welcome page

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Landing from "./components/Landing";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <Landing />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ client/src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="bg-dark text-white mt-5 p-4 text-center">
+      Houngue Yaovi &copy; {new Date().getFullYear()}
+    </footer>
+  );
+}
+export default Footer;
