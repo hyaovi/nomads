1473289457fce559121de695d8bd0c9197241cad
hyaovi <hfyakre@gmail.com>
2018-12-09 14:58:24 +0300

    implementing redux

--- client/src/App.js
+++ client/src/App.js
@@ -5,22 +5,27 @@ import Footer from "./components/layouts/Footer";
 import Landing from "./components/layouts/Landing";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
+import { Provider } from "react-redux";
+import store from "./store";
+
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-        <div className="App">
-          <Navbar />
-          <Route path="/" component={Landing} exact />
-          <div className="container">
-            <Route path="/login" component={Login} exact />
-            <Route path="/Register" component={Register} exact />
+      <Provider store={store}>
+        <BrowserRouter>
+          <div className="App">
+            <Navbar />
+            <Route path="/" component={Landing} exact />
+            <div className="container">
+              <Route path="/login" component={Login} exact />
+              <Route path="/Register" component={Register} exact />
+            </div>
+            <Footer />
           </div>
-          <Footer />
-        </div>
-      </BrowserRouter>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
---
+++ client/src/store.js
@@ -0,0 +1,4 @@
+import { createStore, applyMiddleware } from "redux";
+const store = createStore(() => [], {}, applyMiddleware());
+
+export default store;
