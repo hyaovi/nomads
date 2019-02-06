c689cd9016d3f002c6928c02c65bff8ef88de5e5
hyaovi <hfyakre@gmail.com>
2019-02-06 07:35:59 +0300

    set profile header

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -16,13 +16,32 @@ class Profile extends Component {
     }
   }
   render() {
+    const { profile, loading } = this.props.profile;
+    let profileContent;
+    if (profile === null || loading) {
+      profileContent = <Spinner />;
+    } else {
+      profileContent = (
+        <div>
+          <div className="row">
+            <div className="col-md-6 float-left">
+              <Link to="/profiles" className="btn btn-light mb-3">
+                Go back
+              </Link>
+            </div>
+            <div className="col-md-6" />
+          </div>
+          <ProfileHeader profile={profile} />
+        </div>
+      );
+    }
     return (
-      <div>
-        <h1 className="display-4">Profile</h1>
-        <ProfileHeader />
-        <ProfileAbout />
-        <ProfileCreds />
-        <ProfileGithub />
+      <div className="profile">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">{profileContent}</div>
+          </div>
+        </div>
       </div>
     );
   }
