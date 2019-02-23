389438d5394992756dc942e9f99bea4245ea0c8b
hyaovi <hfyakre@gmail.com>
2019-02-23 10:01:57 +0300

    deplayment part 3

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
-const app = express();
+const path = require("path");
 
+const app = express();
 //body-parser
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -32,15 +33,11 @@ const users = require("./routes/api/users");
 const posts = require("./routes/api/posts");
 const profile = require("./routes/api/profile");
 
-mongoose.connect(
-  dbURI,
-  { useNewUrlParser: true },
-  (err, db) => {
-    if (err) {
-      console.log(`WE GOT ISSUES: ${err} `);
-    } else console.log("db connected!");
-  }
-);
+mongoose.connect(dbURI, { useNewUrlParser: true }, (err, db) => {
+  if (err) {
+    console.log(`WE GOT ISSUES: ${err} `);
+  } else console.log("db connected!");
+});
 
 app.get("/", (req, res) => {
   res.send("hello!");
@@ -50,6 +47,15 @@ app.get("/", (req, res) => {
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
+
+//for prod deployment
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
