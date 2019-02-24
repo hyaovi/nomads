2bb9547c64a984e96615927bb08d1f20440733c1
hyaovi <hfyakre@gmail.com>
2019-02-24 17:31:02 +0300

    added react build

---
+++ client/build/precache-manifest.176d1bb1ee2f6557f285730cd3dbf686.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b26d8256bdf1854cc67b47777212a45e",
+    "url": "/static/media/showcase.b26d8256.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d8863e3f495373cf6ea3",
+    "url": "/static/js/main.d8863e3f.chunk.js"
+  },
+  {
+    "revision": "c7efeb50454b6080dc84",
+    "url": "/static/js/1.c7efeb50.chunk.js"
+  },
+  {
+    "revision": "d8863e3f495373cf6ea3",
+    "url": "/static/css/main.23cba11e.chunk.css"
+  },
+  {
+    "revision": "63ae8f74d07045a054629cb2247780b2",
+    "url": "/index.html"
+  }
+];
