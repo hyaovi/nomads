07f30f04d0b978f059c84606d71c7720b2c9a910
hyaovi <hfyakre@gmail.com>
2019-02-24 16:10:10 +0300

    deployment hard knock life

--- server.js
+++ server.js
@@ -47,11 +47,15 @@ app.use("/api/posts", posts);
 //for prod deployment
 if (process.env.NODE_ENV === "production") {
   //set static folder
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
+  app.use(express.static(path.join(__dirname, "client", "build")));
+  app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
+
+app.get("/", (req, res) => {
+  res.json("hello");
+});
 
 const port = process.env.PORT || 5000;
 
