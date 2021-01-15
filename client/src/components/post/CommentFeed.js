dd4433d2e47e9a6af3b5ae5994d8bab1d117c237
Houngue Yaovi <hfyaovi@gmail.com>
2021-01-16 01:25:32 +0300

    minor testing function component

--- client/src/components/post/CommentFeed.js
+++ client/src/components/post/CommentFeed.js
@@ -1,15 +1,23 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import CommentItem from "./CommentItem";
 
-class CommentFeed extends Component {
-  render() {
-    const { comments, postID } = this.props;
-    return comments.map(comment => (
-      <CommentItem key={comment._id} comment={comment} postID={postID} />
-    ));
-  }
+// class CommentFeed extends Component {
+//   render() {
+//     const { comments, postID } = this.props;
+//     return comments.map(comment => (
+//       <CommentItem key={comment._id} comment={comment} postID={postID} />
+//     ));
+//   }
+// }
+
+function CommentFeed({comment, postID}) {
+  return (
+    <CommentItem key={comment._id} comment={comment} postID={postID} />
+  )
 }
+
+
 CommentFeed.propTypes = {
   comments: PropTypes.array.isRequired,
   postID: PropTypes.string.isRequired
